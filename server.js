459ced3629fa1a45bc282f7b022ac4080e9a1bc2
Tiger Ji <tiger.j540@gmail.com>
2022-08-07 16:58:28 -0500

    testing without router

--- server.js
+++ server.js
@@ -36,3 +36,14 @@ app.listen(port, () => {
 
     console.log(`Server is running on port: ${port}`);
 });
+
+
+app.get(("/check-username/:username"), (req, response) => {
+    let db_connect = dbo.getDb();
+    let my_query = { username: req.params.username };
+
+    db_connect.collection("users").findOne(my_query, (err, res) => {
+        if (err) throw err;
+        return response.json(res);
+    })
+});
